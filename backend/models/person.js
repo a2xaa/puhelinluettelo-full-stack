86f363608bd99d856e9f07b482b2e59de640c144
a2xaa <156316871+a2xaa@users.noreply.github.com>
2025-09-28 17:09:31 +0300

    Update MongoDB connection for phonebook

--- backend/models/person.js
+++ backend/models/person.js
@@ -4,6 +4,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
   .then(result => {
     console.log('connected to MongoDB')
