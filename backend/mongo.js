3742ac9c3191018668d86b5ffe588dff15819689
a2xaa <156316871+a2xaa@users.noreply.github.com>
2025-09-28 17:17:40 +0300

    Updated mongo.js

--- backend/mongo.js
+++ backend/mongo.js
@@ -7,47 +7,24 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://teomaen:${password}@cluster0.hhatalk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
 })
 
-const Person = mongoose.model('Person', personSchema)
+const Note = mongoose.model('Note', noteSchema)
 
-if (process.argv.length === 3) {
-  // Fetch all persons from database
-  console.log('phonebook:')
-  Person.find({}).then(result => {
-    result.forEach(person => {
-      console.log(person.name, person.number)
-    })
-    mongoose.connection.close()
-  })
-} else if (process.argv.length === 5) {
-  const name = process.argv[3]
-  const number = process.argv[4]
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
 
-  // Create a new person object using the Person model (constructor function)
-  const person = new Person({
-    name: name,
-    number: number,
-  })
-
-  // Save the object to database using the save method
-  // The save method returns a promise
-  person.save().then(result => {
-    console.log(`added ${name} number ${number} to phonebook`)
-    console.log('Saved person:', result)
-    mongoose.connection.close()
-  })
-} else {
-  console.log('Usage:')
-  console.log('  node mongo.js <password> - list all entries')
-  console.log('  node mongo.js <password> <name> <number> - add new entry')
+note.save().then(result => {
+  console.log('note saved!')
   mongoose.connection.close()
-}
+})
